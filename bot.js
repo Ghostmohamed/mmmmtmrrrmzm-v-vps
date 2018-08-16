c12373cd65a677543d822d1a2ecd70b6c2bd5a00
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-16 16:38:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,28 @@ client.channels.find('id', '478949105771610112').setName("Welcome To DMR");
 });
 
 
-
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc1') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
 
 
 
