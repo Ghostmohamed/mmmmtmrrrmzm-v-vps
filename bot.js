17101264f55eb08661bdcf9b2ad7e1c22de27f95
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-16 16:23:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,67 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 });
 
 
+
+
+client.on('message' , message => {
+    var prefix = "*";
+    if(message.content.startsWith(prefix + 'ticket')) {
+        message.channel.send('The Ticket has been create');
+        client.users.get('347048358529204226').send({embed: new Discord.RichEmbed()
+        .setDescription(**<@${message.author.id}> has Create a Ticket call him**)
+        });
+    }
+});
+
+
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    var prefix = ".";
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return message.channel.send('هذا الأمر فقط للسيرفرات').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('للأسف لا تمتلك صلاحية ADMINISTRATOR' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "BY XMAS";
+    let request = Requested By ${message.author.username};
+    if (!args) return message.reply('
+يجب عليك كتابة كلمة او جملة لإرسال البرودكاست
+');message.channel.send(**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \ ${args}``).then(msg => {
+    msg.react(':white_check_mark:')
+    .then(() => msg.react(':x:'))
+    .then(() =>msg.react(':white_check_mark:'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === ':white_check_mark:' && user.id === message.author.id;
+
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === ':x:' && user.id === message.author.id;
+
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+    message.channel.send(☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('Broadcast')
+       .addField('Server', message.guild.name)
+       .addField('Sender', message.author.username)
+       .addField('Message', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(**Broadcast Canceled.**).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
+
+
+
 client.on("ready", async  => {
 setInterval(function(){
 client.channels.find('id', '478949105771610112').setName("W");
