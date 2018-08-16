54a08ed8e02f3a23a0d73c3b835df57dedec133f
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-16 16:59:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,6 +80,36 @@ return;
 
 
 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`عضو جديد`)
+    .setDescription(`اهلا بك في السيرفر`)
+    .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('GREEN')
+    .setFooter('The King Bot', 'https://cdn.discordapp.com/icons/390551815072251904/418fa2788d8115808951c9881ba8f190.jpg')
+
+var channel =member.guild.channels.find('name', 'wlc')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
