15cfc322828b32cda39b4e26449db6271b86034a
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-18 23:24:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -612,7 +612,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('message', message => {
-if (message.content === '.spam') {
+if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
