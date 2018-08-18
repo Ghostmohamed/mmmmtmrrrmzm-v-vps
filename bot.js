613432a599ffebf2eae469ada90f1053606e0d45
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-18 23:25:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -608,24 +608,6 @@ client.on('message', function(msg) {
 
 
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on('message', message => {
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client.login("NDc5NTk1ODkzNTM3MTEyMDc0.DloelQ.O2JXdbmX-9XHfjnZquOlzNd_ayQ");
 
 
 
