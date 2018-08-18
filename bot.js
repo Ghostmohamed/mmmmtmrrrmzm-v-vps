65c42c85c1cc50c601160723e1355d021677eeae
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-18 21:07:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -505,6 +505,34 @@ client.on('message', message => {
 
 
 
+client.on('ready',async () => {
+setInterval(function(){
+var currentTime = new Date(),
+hours = currentTime.getHours() + 3 ,
+ReBeeL = currentTime.getMinutes(),
+ReBeeeL = currentTime.getSeconds(),
+Codes = currentTime.getFullYear(),
+CodeS = currentTime.getMonth() + 1,
+CoDeS = currentTime.getDate()
+if (ReBeeL < 10) {
+ReBeeL = "0" + ReBeeL;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
+client.channels.find('id', '480452489976479754').setName(`Time - ${hours} : ${ReBeeL} : ${ReBeeeL} ${suffix}`) 
+client.channels.find('id', '480452275597213696').setName(`Date : ${Codes} - ${CodeS} - ${CoDeS}`)
+}, 1000);
+});
+
+
+
+
 
 
 
