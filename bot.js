07b09b732891c1a76946b7b21db70a48a42f244c
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-19 02:37:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,6 +158,22 @@ if (message.content.startsWith('صراحة')) {
 
 
 
+client.on('message', function(message) {
+    if(message.content.startsWith(prefix + 'roll')) {
+        let args = message.content.split(" ").slice(1);
+        if (!args[0]) {
+            message.channel.send('**حط رقم معين يتم السحب منه**');
+            return;
+            }
+    message.channel.send(Math.floor(Math.random() * args.join(' ')));
+            if (!args[0]) {
+          message.edit('1')
+          return;
+        }
+    }
+});
+
+
 
 
 
