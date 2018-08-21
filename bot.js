14d64505c619ed827ee034a34861ae40102855f8
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-21 22:31:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -882,7 +882,16 @@ client.on('message', message => {
 
 
 
-
+client.on('ready', () => {
+    console.log(`~~~~~~~~~~~~~~~~~`);
+    console.log(`Logging into Discord`);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`on  ${client.guilds.size} Servers `);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(`سوف اكون جاهز قريبا`,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+ });
 
 
 
