27597dd9f69c1c1c1350eff40d1fb5119291bc67
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-21 22:36:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -791,23 +791,7 @@ client.on('message', msg => {
 
 
 
-  client.on('ready', function(){
-    var ms = 1000 ;
-    var setGame = [`D `,` M`,`R`,];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/idk`);
-    }, ms);1000
 
-});
 
 
 
