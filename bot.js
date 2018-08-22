22ff05b63e65191b08a92926f33d78e58a67666f
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-22 02:37:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -992,7 +992,20 @@ client.on('message', message => {
 
 
 
-
+client.on('message', message => {
+    if (message.content === '!spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+    
 
 
 
