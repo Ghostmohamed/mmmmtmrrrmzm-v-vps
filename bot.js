08b5fc2873fd92b3279c3af7c8867b42d3a811b1
Ghostmohamed <42441943+Ghostmohamed@users.noreply.github.com>
2018-08-29 11:16:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -873,7 +873,7 @@ client.on('ready', () => {
     console.log(`on  ${client.guilds.size} Servers `);
     console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setGame(`عــيد مباركْ لجميع أعضاء كلان دمــآر`,"http://twitch.tv/y04zgamer")
+    client.user.setGame(`نَــحن فـي الخدمــة`,"http://twitch.tv/y04zgamer")
     client.user.setStatus("dnd")
  });
 
